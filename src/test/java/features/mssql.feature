da1882215b8e4aa32bb8b1914297a06b9343ea14
Jon Austen <djangofan@gmail.com>
2020-06-03 22:10:09 -0700

    Simplify example showing jdbc load error.

--- src/test/java/features/mssql.feature
+++ src/test/java/features/mssql.feature
@@ -9,6 +9,6 @@ Feature: mssql query test
     * def config = { username: 'sa', password: 'mssql123', url: 'jdbc:sqlserver://localhost:1433;databaseName=TEST', driverClassName: 'com.microsoft.sqlserver.jdbc.SQLServerDriver' }
     * def DbUtils = Java.type('qa.test.DbUtils')
     * def db = new DbUtils(config)
-    * karate.log(db.readRow("SELECT TOP (1) * FROM [TEST].[dbo].[Order]"))
+    #* karate.log(db.readRow("SELECT TOP (1) * FROM [TEST].[dbo].[Order]"))
 
 
--- src/test/java/qa/test/DbUtils.java
+++ src/test/java/qa/test/DbUtils.java
@@ -2,29 +2,29 @@ package qa.test;
 
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class DbUtils {
     
-    private static final Logger logger = LoggerFactory.getLogger(DbUtils.class); 
+    //private static final Logger logger = LoggerFactory.getLogger(DbUtils.class); 
     
-    private final JdbcTemplate jdbc;
+    private final JdbcTemplate jdbc = null;
 
     public DbUtils(Map<String, Object> config) {
-        String url = (String) config.get("url");
-        String username = (String) config.get("username");
-        String password = (String) config.get("password");
+        //String url = (String) config.get("url");
+        //String username = (String) config.get("username");
+        //String password = (String) config.get("password");
         String driver = (String) config.get("driverClassName");
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(driver);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        jdbc = new JdbcTemplate(dataSource);
-        logger.info("init jdbc template: {}", url);
+        //dataSource.setUrl(url);
+        //dataSource.setUsername(username);
+        //dataSource.setPassword(password);
+        //jdbc = new JdbcTemplate(dataSource);
+        //logger.info("init jdbc template: {}", url);
     }
     
     public Object readValue(String query) {

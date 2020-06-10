8ada73e6d7cab572b57780e3c1beb29a10699c2b
Jon Austen <djangofan@gmail.com>
2020-06-09 21:49:51 -0700

    Minor edit.

--- src/test/java/features/mssql.feature
+++ src/test/java/features/mssql.feature
@@ -3,8 +3,8 @@ Feature: mssql query test
 
   Background: testing
     * def config = { username: 'sa', password: 'mssql123', url: 'jdbc:sqlserver://localhost:1433;databaseName=TEST;user=%s;password=%s', driverClassName: 'com.microsoft.sqlserver.jdbc.SQLServerDriver' }
-    * def DbUtils = Java.type('qa.test.DbUtils')
-    * def db = new DbUtils(config)
+    * def MSSqlUtil = Java.type('qa.test.MSSqlUtil')
+    * def db = new MSSqlUtil(config)
 
   @db
   Scenario: query mssql
@@ -12,5 +12,4 @@ Feature: mssql query test
     * def queryResult = db.queryDB("SELECT TOP (3) * FROM [TEST].[dbo].[Order]")
     * karate.log(queryResult)
     * print "queryResult: " + queryResult
-
-
+    
--- src/test/java/qa/test/MSSqlUtil.java
+++ src/test/java/qa/test/MSSqlUtil.java
@@ -12,37 +12,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class DbUtils {
+public class MSSqlUtil {
 
     private final Map<String, Object> config;
 
-    public DbUtils(final Map<String, Object> configuration) {
+    public MSSqlUtil(final Map<String, Object> configuration) {
         this.config = configuration;
     }
 
-    public List<Map<String, Object>> queryDB(final String query) throws SQLException {
-        final String url = (String) config.get("url");
-        final String username = (String) config.get("username");
-        final String password = (String) config.get("password");
-        final String driverClassName = (String) config.get("driverClassName");
-        final String formattedUrl = String.format(url, username, password);
-
-        try {
-            Driver driver = (Driver) Class.forName(driverClassName).newInstance();
-            // shim is required for debugging with Karate
-            DriverManager.registerDriver(new qa.test.DriverShim(driver));
-
-            // the following did not work
-            // ClassLoader scl = ClassLoader.getSystemClassLoader();
-            //Driver d = (Driver)Class.forName(driverClassName, true, scl).newInstance();
-            //DriverManager.registerDriver(d);
-        } catch (InstantiationException e1) {
-            e1.printStackTrace();
-        } catch (IllegalAccessException e1) {
-            e1.printStackTrace();
-        } catch (ClassNotFoundException e1) {
-            e1.printStackTrace();
-        }
+    public List<Map<String, Object>> queryDB(final String query) {
+        final String formattedUrl = String.format((String)config.get("url"), (String)config.get("username"), (String)config.get("password"));
+        loadDriverOnClasspath();
 
         try (Connection conn = DriverManager.getConnection(formattedUrl);
              Statement stmt = conn.createStatement();
@@ -54,21 +34,38 @@ public class DbUtils {
         }
         return null;
     }
-    
+
     public List<Map<String, Object>> resultSetToArrayList(final ResultSet rs) throws SQLException {
         final ResultSetMetaData md = rs.getMetaData();
         final int columns = md.getColumnCount();
         final List<Map<String, Object>> list = new ArrayList<>();
         while (rs.next()) {
             final Map<String, Object> row = new HashMap<>(columns);
-            for (int i=1; i<=columns; ++i) {           
+            for (int i = 1; i <= columns; ++i) {
                 row.put(md.getColumnName(i), rs.getObject(i));
             }
             list.add(row);
         }
         return list;
-      }
- 
-    
+    }
+
+    private void loadDriverOnClasspath() {
+        String driverClassName = (String) config.get("driverClassName");
+        try {
+            Driver driver = (Driver) Class.forName(driverClassName).newInstance();
+            // shim is required for debugging with Karate
+            DriverManager.registerDriver(new qa.test.DriverShim(driver));
+        } catch (InstantiationException e1) {
+            e1.printStackTrace();
+        } catch (IllegalAccessException e1) {
+            e1.printStackTrace();
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
 }
 
